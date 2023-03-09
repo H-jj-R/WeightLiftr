3069f07c993c9ed5f571adae61ca815e5618f43b
H-jj-R <henrijjroberts@gmail.com>
2023-03-09 20:00:20 +0000

    Added functionality to buttons

--- app/src/main/java/com/example/weightliftr/MainActivity.java
+++ app/src/main/java/com/example/weightliftr/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.weightliftr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button newWorkoutBut = (Button)findViewById(R.id.newWorkoutBut);
+        newWorkoutBut.setOnClickListener(event ->
+                startActivity(new Intent(MainActivity.this, AddNewWorkout.class))
+        );
+
+        Button editWorkoutBut = (Button)findViewById(R.id.editWorkoutBut);
+        editWorkoutBut.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, EditWorkout.class))
+        );
+
+        Button pastWorkoutsBut = (Button)findViewById(R.id.pastWorkoutsBut);
+        pastWorkoutsBut.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, ViewPastWorkouts.class))
+        );
+
+        Button startWorkoutBut = (Button)findViewById(R.id.startWorkoutBut);
+        startWorkoutBut.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, StartWorkout.class))
+        );
     }
 }
