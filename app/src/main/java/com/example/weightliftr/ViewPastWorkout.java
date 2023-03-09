6ef15d70afabd7dc5769afb68c289027638a5201
H-jj-R <henrijjroberts@gmail.com>
2023-03-09 16:02:34 +0000

    Created required activities

---
+++ app/src/main/java/com/example/weightliftr/ViewPastWorkout.java
@@ -0,0 +1,14 @@
+package com.example.weightliftr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewPastWorkout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_past_workout);
+    }
+}
