848b135f61bdfa96d68ed5cebd9f45cff3e3feda
H-jj-R <henrijjroberts@gmail.com>
2023-04-10 19:55:11 +0300

    Added new versions of list item XML

--- app/src/main/java/com/example/weightliftr/ViewPastWorkouts.java
+++ app/src/main/java/com/example/weightliftr/ViewPastWorkouts.java
@@ -41,32 +41,31 @@ public class ViewPastWorkouts extends AppCompatActivity {
         // Get a reference to the LinearLayout
         LinearLayout linearLayout = findViewById(R.id.linearLayout);
 
+        // TODO: Figure out why this doesn't work
         // Inflate the views for each item and add them to the LinearLayout
         for (int i = 0; i < workouts.size(); i++) {
             View view = LayoutInflater.from(this).inflate(R.layout.read_only_list_item, null);
-            TextView textView = view.findViewById(R.id.itemTitle);
-            textView.setText(names.get(i));
-            //List<Exercise> exercises = exerciseListList.get(i);
+            TextView itemTitle = view.findViewById(R.id.itemTitle);
+            itemTitle.setText(names.get(i));
+            List<Exercise> exercises = exerciseListList.get(i);
             linearLayout.addView(view);
 
             // Set an OnClickListener for each item
             view.setOnClickListener(v -> {
-                // Get the TextView inside the clicked view
-                TextView textView1 = v.findViewById(R.id.itemTitle);
+                TextView itemTitle1 = v.findViewById(R.id.itemTitle);
 
-                // Show a Toast message with the text of the clicked item
-                Toast.makeText(ViewPastWorkouts.this, textView1.getText(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(ViewPastWorkouts.this, itemTitle1.getText(), Toast.LENGTH_SHORT).show();
 
                 // Toggle the visibility of the extra details for the clicked item
                 View extraDetails = v.findViewById(R.id.extraDetails);
-                //TextView extraDetailsTextView = findViewById(R.id.extraDetails);
+                TextView extraDetailsTextView = findViewById(R.id.extraDetails);
                 if (extraDetails.getVisibility() == View.GONE) {
-//                    for (int j = 0; j < exercises.size(); j++) {
-//                        extraDetailsTextView.setText((String) extraDetailsTextView.getText() + exercises.get(j).getName() + "\n");
-//                    }
+                    for (int j = 0; j < exercises.size(); j++) {
+                        extraDetailsTextView.setText((String) extraDetailsTextView.getText() + exercises.get(j).getName() + "\n");
+                    }
                     extraDetails.setVisibility(View.VISIBLE);
                 } else {
-                    //extraDetailsTextView.setText("");
+                    extraDetailsTextView.setText("");
                     extraDetails.setVisibility(View.GONE);
                 }
             });
